d33db7e9c08b5c75f4db154bf9c3249f46396344
Rohit Singh <rohit21oct85@gmail.com>
2021-09-03 10:28:14 +0530

    change port

--- src/config/config.ts
+++ src/config/config.ts
@@ -24,7 +24,7 @@ const MONGO = {
 }
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost'
-const SERVER_PORT = process.env.SERVER_PORT || '3001'
+const SERVER_PORT = process.env.SERVER_PORT || '3000'
 
 const SERVER = {
       hostname: SERVER_HOSTNAME,
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { app } from './app';
 import * as http from 'http';
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const host = `${'0.0.0.0'}:${port}`;
 
 server.listen(host)
